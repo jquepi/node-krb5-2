41ebf5c18bdbe2068159f8359357c4d75708f155
Leonardo Gatica <lgatica@protonmail.com>
2016-12-04 01:50:19 -0300

    chore(build): Update build commands

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-docker build -t node-krb5:7.1.0 7.1.0
-docker tag node-krb5:7.1.0 lgatica/node-krb5
-docker tag node-krb5:7.1.0 lgatica/node-krb5:7.1.0
-docker tag node-krb5:7.1.0 lgatica/node-krb5:7.1
-docker tag node-krb5:7.1.0 lgatica/node-krb5:7
-docker tag node-krb5:7.1.0 lgatica/node-krb5:latest
+docker build -t node-krb5:7.2.0 7.2.0
+docker tag node-krb5:7.2.0 lgatica/node-krb5
+docker tag node-krb5:7.2.0 lgatica/node-krb5:7.2.0
+docker tag node-krb5:7.2.0 lgatica/node-krb5:7.2
+docker tag node-krb5:7.2.0 lgatica/node-krb5:7
+docker tag node-krb5:7.2.0 lgatica/node-krb5:latest
 docker build -t node-krb5:6.9.1 6.9.1
 docker tag node-krb5:6.9.1 lgatica/node-krb5:6.9.1
 docker tag node-krb5:6.9.1 lgatica/node-krb5:6.9
@@ -12,11 +12,11 @@ docker build -t node-krb5:4.6.2 4.6.2
 docker tag node-krb5:4.6.2 lgatica/node-krb5:4.6.2
 docker tag node-krb5:4.6.2 lgatica/node-krb5:4.6
 docker tag node-krb5:4.6.2 lgatica/node-krb5:4
-docker build -t node-krb5:7.1.0-onbuild 7.1.0/onbuild
-docker tag node-krb5:7.1.0-onbuild lgatica/node-krb5:7.1.0-onbuild
-docker tag node-krb5:7.1.0-onbuild lgatica/node-krb5:7.1-onbuild
-docker tag node-krb5:7.1.0-onbuild lgatica/node-krb5:7-onbuild
-docker tag node-krb5:7.1.0-onbuild lgatica/node-krb5:onbuild
+docker build -t node-krb5:7.2.0-onbuild 7.2.0/onbuild
+docker tag node-krb5:7.2.0-onbuild lgatica/node-krb5:7.2.0-onbuild
+docker tag node-krb5:7.2.0-onbuild lgatica/node-krb5:7.2-onbuild
+docker tag node-krb5:7.2.0-onbuild lgatica/node-krb5:7-onbuild
+docker tag node-krb5:7.2.0-onbuild lgatica/node-krb5:onbuild
 docker build -t node-krb5:6.9.1-onbuild 6.9.1/onbuild
 docker tag node-krb5:6.9.1-onbuild lgatica/node-krb5:6.9.1-onbuild
 docker tag node-krb5:6.9.1-onbuild lgatica/node-krb5:6.9-onbuild
