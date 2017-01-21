d37de4875094d61d0e6f7bd64d7f7964d967dd7c
Leonardo Gatica <lgatica@protonmail.com>
2017-01-21 16:40:10 -0300

    feat(node): Update node from v6.9.3 to v6.9.4
    Remove MAINTAINER and add LABEL maintainer

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ docker tag node-krb5:7.4.0 lgatica/node-krb5:7.4.0
 docker tag node-krb5:7.4.0 lgatica/node-krb5:7.4
 docker tag node-krb5:7.4.0 lgatica/node-krb5:7
 docker tag node-krb5:7.4.0 lgatica/node-krb5:latest
-docker build -t node-krb5:6.9.3 6.9.3
-docker tag node-krb5:6.9.3 lgatica/node-krb5:6.9.3
-docker tag node-krb5:6.9.3 lgatica/node-krb5:6.9
-docker tag node-krb5:6.9.3 lgatica/node-krb5:6
+docker build -t node-krb5:6.9.4 6.9.4
+docker tag node-krb5:6.9.4 lgatica/node-krb5:6.9.4
+docker tag node-krb5:6.9.4 lgatica/node-krb5:6.9
+docker tag node-krb5:6.9.4 lgatica/node-krb5:6
 docker build -t node-krb5:4.7.1 4.7.1
 docker tag node-krb5:4.7.1 lgatica/node-krb5:4.7.1
 docker tag node-krb5:4.7.1 lgatica/node-krb5:4.7
@@ -17,10 +17,10 @@ docker tag node-krb5:7.4.0-onbuild lgatica/node-krb5:7.4.0-onbuild
 docker tag node-krb5:7.4.0-onbuild lgatica/node-krb5:7.4-onbuild
 docker tag node-krb5:7.4.0-onbuild lgatica/node-krb5:7-onbuild
 docker tag node-krb5:7.4.0-onbuild lgatica/node-krb5:onbuild
-docker build -t node-krb5:6.9.3-onbuild 6.9.3/onbuild
-docker tag node-krb5:6.9.3-onbuild lgatica/node-krb5:6.9.3-onbuild
-docker tag node-krb5:6.9.3-onbuild lgatica/node-krb5:6.9-onbuild
-docker tag node-krb5:6.9.3-onbuild lgatica/node-krb5:6-onbuild
+docker build -t node-krb5:6.9.4-onbuild 6.9.4/onbuild
+docker tag node-krb5:6.9.4-onbuild lgatica/node-krb5:6.9.4-onbuild
+docker tag node-krb5:6.9.4-onbuild lgatica/node-krb5:6.9-onbuild
+docker tag node-krb5:6.9.4-onbuild lgatica/node-krb5:6-onbuild
 docker build -t node-krb5:4.7.1-onbuild 4.7.1/onbuild
 docker tag node-krb5:4.7.1-onbuild lgatica/node-krb5:4.7.1-onbuild
 docker tag node-krb5:4.7.1-onbuild lgatica/node-krb5:4.7-onbuild
