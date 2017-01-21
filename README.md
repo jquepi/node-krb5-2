1ce5610947e7bb0400d0d6484f52284dc620bbad
Leonardo Gatica <lgatica@protonmail.com>
2017-01-21 16:42:36 -0300

    feat(node): Update node from v4.7.1 to v4.7.2
    Remove MAINTAINER and add LABEL maintainer

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Supported tags and respective Dockerfile links
 - 7.4.0-onbuild, 7.4-onbuild, 7-onbuild, onbuild ([7.4/onbuild/Dockerfile](https://github.com/lgaticaq/node-krb5/blob/master/7.4.0/onbuild/Dockerfile))
 - 6.9.4, 6.9, 6 ([6.9/Dockerfile](https://github.com/lgaticaq/node-krb5/blob/master/6.9.4/Dockerfile))
 - 6.9.4-onbuild, 6.9-onbuild, 6-onbuild ([6.9/onbuild/Dockerfile](https://github.com/lgaticaq/node-krb5/blob/master/6.9.4/onbuild/Dockerfile))
-- 4.7.1, 4.7, 4 ([4.7/Dockerfile](https://github.com/lgaticaq/node-krb5/blob/master/4.7.1/Dockerfile))
-- 4.7.1-onbuild, 4.7-onbuild, 4-onbuild ([4.7/onbuild/Dockerfile](https://github.com/lgaticaq/node-krb5/blob/master/4.7.1/onbuild/Dockerfile))
+- 4.7.2, 4.7, 4 ([4.7/Dockerfile](https://github.com/lgaticaq/node-krb5/blob/master/4.7.2/Dockerfile))
+- 4.7.2-onbuild, 4.7-onbuild, 4-onbuild ([4.7/onbuild/Dockerfile](https://github.com/lgaticaq/node-krb5/blob/master/4.7.2/onbuild/Dockerfile))
 
 ## Create a Dockerfile in your Node.js app project
 ```dockerfile
--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ docker build -t node-krb5:6.9.4 6.9.4
 docker tag node-krb5:6.9.4 lgatica/node-krb5:6.9.4
 docker tag node-krb5:6.9.4 lgatica/node-krb5:6.9
 docker tag node-krb5:6.9.4 lgatica/node-krb5:6
-docker build -t node-krb5:4.7.1 4.7.1
-docker tag node-krb5:4.7.1 lgatica/node-krb5:4.7.1
-docker tag node-krb5:4.7.1 lgatica/node-krb5:4.7
-docker tag node-krb5:4.7.1 lgatica/node-krb5:4
+docker build -t node-krb5:4.7.2 4.7.2
+docker tag node-krb5:4.7.2 lgatica/node-krb5:4.7.2
+docker tag node-krb5:4.7.2 lgatica/node-krb5:4.7
+docker tag node-krb5:4.7.2 lgatica/node-krb5:4
 docker build -t node-krb5:7.4.0-onbuild 7.4.0/onbuild
 docker tag node-krb5:7.4.0-onbuild lgatica/node-krb5:7.4.0-onbuild
 docker tag node-krb5:7.4.0-onbuild lgatica/node-krb5:7.4-onbuild
@@ -21,8 +21,8 @@ docker build -t node-krb5:6.9.4-onbuild 6.9.4/onbuild
 docker tag node-krb5:6.9.4-onbuild lgatica/node-krb5:6.9.4-onbuild
 docker tag node-krb5:6.9.4-onbuild lgatica/node-krb5:6.9-onbuild
 docker tag node-krb5:6.9.4-onbuild lgatica/node-krb5:6-onbuild
-docker build -t node-krb5:4.7.1-onbuild 4.7.1/onbuild
-docker tag node-krb5:4.7.1-onbuild lgatica/node-krb5:4.7.1-onbuild
-docker tag node-krb5:4.7.1-onbuild lgatica/node-krb5:4.7-onbuild
-docker tag node-krb5:4.7.1-onbuild lgatica/node-krb5:4-onbuild
+docker build -t node-krb5:4.7.2-onbuild 4.7.2/onbuild
+docker tag node-krb5:4.7.2-onbuild lgatica/node-krb5:4.7.2-onbuild
+docker tag node-krb5:4.7.2-onbuild lgatica/node-krb5:4.7-onbuild
+docker tag node-krb5:4.7.2-onbuild lgatica/node-krb5:4-onbuild
 docker push lgatica/node-krb5
